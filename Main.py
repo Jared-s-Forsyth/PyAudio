d4837e61d85a34f3d748deda2ef853d173bb7a45
Jared Forsyth <Jared.s.forsyth@gmail.com>
2024-04-10 06:35:18 +1200

    Commit 10/4 On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: I hate this in kine text editor let me out

--- Main.py
+++ Main.py
@@ -3,5 +3,11 @@ import pandas as pd
 import bs4 as bs
 import matplotlib as mpl
 import selenium as sm
-
-print("lin")
+import pyaudio as pa
+import torch
+from tkinter import * 
+gui = Tk()
+gui.title("Hello World")
+gui.geometry('900x150')
+gui.tk.call('tk','scaling',3.0)
+gui.mainloop()
